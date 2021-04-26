ebfb35c1faad4f38891689c849ae52db919d920e
ianfox1983 <ianfox1983@gmail.com>
2021-04-26 16:50:09 +0000

    terminado sin estilos

--- src/app.js
+++ src/app.js
@@ -1,95 +1,178 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-const NUMBERS = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "J",
-  "Q",
-  "K"
-  /* "<img src='https://media.istockphoto.com/vectors/dancing-joker-with-playing-cards-on-white-vector-id960303126' style='width: 250px; height: 250px;'>"*/
-];
+const JOKER =
+  "<img src='https://media.istockphoto.com/vectors/dancing-joker-with-playing-cards-on-white-vector-id960303126' style='width: 2em; height: 2em;'>";
+const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 const ICONS = ["♥", "♠", "♦", "♣"];
 const COLORS = ["black", "red"];
+
+const CARD_LIST = document.querySelector("#cardList");
+const CARD_LIST_ORDERED = document.querySelector("#cardListOrdered");
 const TOP_SYMBOL = document.querySelector("#topSymbol");
 const NUMBER = document.querySelector("#number");
 const BOTTOM_SYMBOL = document.querySelector("#bottomSymbol");
 const INPUT = document.querySelector("#NumberOfCards");
 const GENERATE_BUTTON = document.querySelector("#generatorButton");
-const SORT_BUTTON = document.querySelector("#sortButton");
+const SORT_BUTTON_BUBBLE = document.querySelector("#sortButtonBubble");
+const SORT_BUTTON_SELECT = document.querySelector("#sortButtonSelect");
+
+let cardvalue = {
+  symbol: "",
+  number: "",
+  color: ""
+};
+let cardList = [];
+let cardListBubble = [];
 
 window.onload = function() {
   generateCards();
-
-  /* generateValue();
-  document.querySelector("#button").addEventListener("click", event => {
-    // Al hacer click hace un a nueva llamada y genera un nuevo valor
-    generateValue();
-  });
-*/
+  sortCardsBubble();
+  sortCardsSelect();
 };
 
 function randomElement(myArray) {
+  // función que devuelve un elemento aleatorio de un array dado
   return myArray[Math.floor(Math.random() * myArray.length)];
-}
-function generateValue() {
-  let Symbol = randomElement(ICONS);
-  let Color = randomElement(COLORS);
-  let Number = randomElement(NUMBERS);
-  topSymbol.innerHTML = Symbol;
-  topSymbol.style.color = Color;
-  bottomSymbol.innerHTML = Symbol;
-  bottomSymbol.style.color = Color;
-  number.innerHTML = Number;
-  if (
-    Number ==
-    "<img src='https://media.istockphoto.com/vectors/dancing-joker-with-playing-cards-on-white-vector-id960303126' style='width: 250px; height: 250px;'>"
-  ) {
-    topSymbol.innerHTML = "";
-    bottomSymbol.innerHTML = "";
-  }
-}
-
-function generateValue1(myArray) {
-  randomElement();
 }
 
 function generateCards() {
+  //función que genera nuevas cartas cuando se pulsa el botón de generar
   let numberOfCards = "";
   GENERATE_BUTTON.addEventListener("click", event => {
     numberOfCards = INPUT.value;
     console.log(numberOfCards);
-    document.querySelector("section").innerHTML = "";
+    CARD_LIST.innerHTML = "";
+    CARD_LIST_ORDERED.innerHTML = "";
+    cardList = [];
     for (let index = 0; index < numberOfCards; index++) {
-      drawCard();
+      let cardTemp = getCard();
+      cardList.push(cardTemp);
+      drawCard(cardTemp, CARD_LIST);
     }
+    console.log(cardList);
   });
 }
 
-function drawCard() {
+function getCard() {
+  //función que genera el valor de una carta (simbolo, número y color)
+  let cardAux = {
+    symbol: "",
+    number: "",
+    color: ""
+  };
+  cardAux.symbol = randomElement(ICONS);
+  cardAux.number = randomElement(NUMBERS);
+  cardAux.color = randomElement(COLORS);
+  return cardAux;
+}
+
+function drawCard(myObject, place) {
+  //función que imprime la fila (array) de cartas en pantalla
   let topBox = document.createElement("div");
   let centerBox = document.createElement("div");
   let bottomBox = document.createElement("div");
   let card = document.createElement("div");
+
   topBox.id = "topBox";
   topBox.classList.add("topBox");
-  topBox.innerHTML = randomElement(ICONS);
+  topBox.innerHTML = myObject.symbol;
+  topBox.style.color = myObject.color;
   centerBox.id = "centerBox";
   centerBox.classList.add("centerBox");
-  centerBox.innerHTML = randomElement(NUMBERS);
+  if (
+    myObject.number != 1 &&
+    myObject.number != 11 &&
+    myObject.number != 12 &&
+    myObject.number != 13 &&
+    myObject.number != 14
+  ) {
+    centerBox.innerHTML = myObject.number;
+  }
+  if (myObject.number == 1) {
+    centerBox.innerHTML = "A";
+  }
+  if (myObject.number == 11) {
+    centerBox.innerHTML = "J";
+  }
+  if (myObject.number == 12) {
+    centerBox.innerHTML = "Q";
+  }
+  if (myObject.number == 13) {
+    centerBox.innerHTML = "K";
+  }
+  if (myObject.number == 14) {
+    centerBox.innerHTML = JOKER;
+  }
+
   bottomBox.id = "bottomBox";
   bottomBox.classList.add("bottomBox");
-  bottomBox.innerHTML = randomElement(ICONS);
+  bottomBox.innerHTML = myObject.symbol;
+  bottomBox.style.color = myObject.color;
+  if (myObject.number == 14) {
+    topBox.innerHTML = "";
+    bottomBox.innerHTML = "";
+  }
   card.id = "card";
   card.appendChild(topBox);
   card.appendChild(centerBox);
   card.appendChild(bottomBox);
-  document.querySelector("section").appendChild(card);
+  place.appendChild(card);
+}
+
+function bubbleSort(arr) {
+  console.log("hola");
+  let wall = arr.length - 1; //we start the wall at the end of the array
+  while (wall > 0) {
+    let index = 0;
+    while (index < wall) {
+      //compare the adjacent positions, if the right one is bigger, we have to swap
+      if (arr[index].number > arr[index + 1].number) {
+        let aux = arr[index];
+        arr[index] = arr[index + 1];
+        arr[index + 1] = aux;
+      }
+      index++;
+    }
+    wall--; //decrease the wall for optimization
+  }
+  console.log(arr);
+  return arr;
+}
+
+function sortCardsBubble() {
+  SORT_BUTTON_BUBBLE.addEventListener("click", event => {
+    bubbleSort(cardList);
+    CARD_LIST_ORDERED.innerHTML = "";
+    CARD_LIST_ORDERED.innerHTML = "<p>Cartas ordenadas:</p>";
+    for (let index = 0; index < cardList.length; index++) {
+      drawCard(cardList[index], CARD_LIST_ORDERED);
+    }
+  });
+}
+
+function selectSort(arr) {
+  let min = 0;
+  while (min < arr.length - 1) {
+    for (let i = min + 1; i < arr.length - 1; i++) {
+      if (arr[min].number > arr[i].number) {
+        let aux = arr[min];
+        arr[min] = arr[i];
+        arr[i] = aux;
+      }
+    }
+    min++;
+  }
+  return arr;
+}
+
+function sortCardsSelect() {
+  SORT_BUTTON_SELECT.addEventListener("click", event => {
+    selectSort(cardList);
+    CARD_LIST_ORDERED.innerHTML = "";
+    CARD_LIST_ORDERED.innerHTML = "<p>Cartas ordenadas:</p>";
+    for (let index = 0; index < cardList.length; index++) {
+      drawCard(cardList[index], CARD_LIST_ORDERED);
+    }
+  });
 }
