f9b325b4e4443a8e53f97b27e307e85406d39494
kholostyak17 <kholostyak17@gmail.com>
2021-04-27 07:11:27 +0000

    added rows of cards and function to print, & improved all algoritms

--- src/app.js
+++ src/app.js
@@ -9,49 +9,41 @@ const COLORS = ["black", "red"];
 
 const CARD_LIST = document.querySelector("#cardList");
 const CARD_LIST_ORDERED = document.querySelector("#cardListOrdered");
-const TOP_SYMBOL = document.querySelector("#topSymbol");
-const NUMBER = document.querySelector("#number");
-const BOTTOM_SYMBOL = document.querySelector("#bottomSymbol");
 const INPUT = document.querySelector("#NumberOfCards");
 const GENERATE_BUTTON = document.querySelector("#generatorButton");
 const SORT_BUTTON_BUBBLE = document.querySelector("#sortButtonBubble");
 const SORT_BUTTON_SELECT = document.querySelector("#sortButtonSelect");
 
-let cardvalue = {
-  symbol: "",
-  number: "",
-  color: ""
-};
 let cardList = [];
 let cardListBubble = [];
 
 window.onload = function() {
-  generateCards();
+  generateNewCards();
   sortCardsBubble();
   sortCardsSelect();
 };
 
-function randomElement(myArray) {
-  // función que devuelve un elemento aleatorio de un array dado
-  return myArray[Math.floor(Math.random() * myArray.length)];
-}
-
-function generateCards() {
+function generateNewCards() {
   //función que genera nuevas cartas cuando se pulsa el botón de generar
   let numberOfCards = "";
   GENERATE_BUTTON.addEventListener("click", event => {
-    numberOfCards = INPUT.value;
-    console.log(numberOfCards);
-    CARD_LIST.innerHTML = "";
+    numberOfCards = INPUT.value; //recoge dato del input
+    CARD_LIST.innerHTML = ""; //vacía div y borra cartas anteriores
     CARD_LIST_ORDERED.innerHTML = "";
-    cardList = [];
+    cardList = []; //vacía array y borra cartas anteriores
+    let row = document.createElement("div");
     for (let index = 0; index < numberOfCards; index++) {
       let cardTemp = getCard();
       cardList.push(cardTemp);
-      drawCard(cardTemp, CARD_LIST);
+      drawCard(cardTemp, CARD_LIST, row);
     }
     console.log(cardList);
   });
+}
+
+function randomElement(myArray) {
+  // función que devuelve un elemento aleatorio de un array dado
+  return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 function getCard() {
@@ -67,7 +59,7 @@ function getCard() {
   return cardAux;
 }
 
-function drawCard(myObject, place) {
+function drawCard(myObject, place, row) {
   //función que imprime la fila (array) de cartas en pantalla
   let topBox = document.createElement("div");
   let centerBox = document.createElement("div");
@@ -117,12 +109,39 @@ function drawCard(myObject, place) {
   card.appendChild(topBox);
   card.appendChild(centerBox);
   card.appendChild(bottomBox);
-  place.appendChild(card);
+  row.classList.add("row");
+  row.appendChild(card);
+  place.appendChild(row);
+}
+
+function drawRowOfCards(myArray, index) {
+  let row = document.createElement("div");
+  row.innerHTML = index;
+  for (let index = 0; index < cardList.length; index++) {
+    drawCard(myArray[index], CARD_LIST_ORDERED, row);
+  }
+}
+
+function sortCardsBubble() {
+  SORT_BUTTON_BUBBLE.addEventListener("click", event => {
+    CARD_LIST_ORDERED.innerHTML = "";
+    CARD_LIST_ORDERED.innerHTML = "<p>Cartas ordenadas:</p>";
+    bubbleSort(cardList);
+  });
+}
+
+function sortCardsSelect() {
+  SORT_BUTTON_SELECT.addEventListener("click", event => {
+    CARD_LIST_ORDERED.innerHTML = "";
+    CARD_LIST_ORDERED.innerHTML = "<p>Cartas ordenadas:</p>";
+    selectSort(cardList);
+  });
 }
 
 function bubbleSort(arr) {
   console.log("hola");
   let wall = arr.length - 1; //we start the wall at the end of the array
+  let contador = 0;
   while (wall > 0) {
     let index = 0;
     while (index < wall) {
@@ -132,6 +151,8 @@ function bubbleSort(arr) {
         arr[index] = arr[index + 1];
         arr[index + 1] = aux;
       }
+      drawRowOfCards(cardList, contador);
+      contador++;
       index++;
     }
     wall--; //decrease the wall for optimization
@@ -140,19 +161,9 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function sortCardsBubble() {
-  SORT_BUTTON_BUBBLE.addEventListener("click", event => {
-    bubbleSort(cardList);
-    CARD_LIST_ORDERED.innerHTML = "";
-    CARD_LIST_ORDERED.innerHTML = "<p>Cartas ordenadas:</p>";
-    for (let index = 0; index < cardList.length; index++) {
-      drawCard(cardList[index], CARD_LIST_ORDERED);
-    }
-  });
-}
-
 function selectSort(arr) {
   let min = 0;
+  let contador = 0;
   while (min < arr.length - 1) {
     for (let i = min + 1; i < arr.length - 1; i++) {
       if (arr[min].number > arr[i].number) {
@@ -160,19 +171,10 @@ function selectSort(arr) {
         arr[min] = arr[i];
         arr[i] = aux;
       }
+      drawRowOfCards(cardList, index);
+      contador++;
     }
     min++;
   }
   return arr;
-}
-
-function sortCardsSelect() {
-  SORT_BUTTON_SELECT.addEventListener("click", event => {
-    selectSort(cardList);
-    CARD_LIST_ORDERED.innerHTML = "";
-    CARD_LIST_ORDERED.innerHTML = "<p>Cartas ordenadas:</p>";
-    for (let index = 0; index < cardList.length; index++) {
-      drawCard(cardList[index], CARD_LIST_ORDERED);
-    }
-  });
 }
